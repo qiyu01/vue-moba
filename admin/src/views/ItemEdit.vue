6fb3ceb9f5d5e9251dc2d82dd399102a7fdfe468
qiyu01 <2452575692@qq.com>
2020-08-31 23:29:29 +0800

    Login-section

--- admin/src/views/ItemEdit.vue
+++ admin/src/views/ItemEdit.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="categoryedit">
+  <div class="itemedit">
     <h1>{{id?'编辑':'新建'}}物品</h1>
     <el-form label-width="120px" @submit.native.prevent="save">
       <el-form-item label="名称">
@@ -61,28 +61,3 @@ export default {
   },
 };
 </script>
-<style>
-  .avatar-uploader .el-upload {
-    border: 1px dashed #d9d9d9;
-    border-radius: 6px;
-    cursor: pointer;
-    position: relative;
-    overflow: hidden;
-  }
-  .avatar-uploader .el-upload:hover {
-    border-color: #409EFF;
-  }
-  .avatar-uploader-icon {
-    font-size: 28px;
-    color: #8c939d;
-    width: 178px;
-    height: 178px;
-    line-height: 178px;
-    text-align: center;
-  }
-  .avatar {
-    width: 178px;
-    height: 178px;
-    display: block;
-  }
-</style>
