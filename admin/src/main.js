7ab7eaa6a341152c9ec7cf1df4b3825ca6ecafe9
qiyu01 <2452575692@qq.com>
2020-09-06 21:15:42 +0800

    backend finish

--- admin/src/main.js
+++ admin/src/main.js
@@ -12,6 +12,21 @@ import http from './http'
 
 Vue.prototype.$http=http
 
+Vue.mixin({
+  computed: {
+    uploadUrl(){
+      return this.$http.defaults.baseURL + '/upload'
+    }
+  },
+  methods: {
+    getAuthHeaders(){
+      return {
+        Authorization:`Bearer ${localStorage.token || ''}`
+      }
+    }
+  },
+})
+
 new Vue({
   router,
   store,
